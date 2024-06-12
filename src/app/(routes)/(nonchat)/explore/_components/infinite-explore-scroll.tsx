7ebeebb79b38da9e9a1f4d145c69e5fcf448d9dd
omprakash1353 <Omprakash13053@gmail.com>
2024-06-12 15:49:44 +0530

    0.0.9

--- src/app/(routes)/(nonchat)/explore/_components/infinite-explore-scroll.tsx
+++ src/app/(routes)/(nonchat)/explore/_components/infinite-explore-scroll.tsx
@@ -1,77 +1,78 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { PostExplorerType } from "@/types/types";
-import { fetchMoreExplorePosts } from "@/actions/post-actions";
-import { Icons } from "@/components/shared/icons";
-import Image from "next/image";
+// import { useEffect } from "react";
+// import Image from "next/image";
+// import { useInView } from "react-intersection-observer";
+// import { useInfiniteQuery } from "@tanstack/react-query";
 
-export function InfiniteExploreScroll({
-  initialPosts,
-}: {
-  initialPosts: PostExplorerType[] | [];
-}) {
-  const [ref, inView] = useInView();
+// import { PostExplorerType } from "@/types/types";
+// import { fetchMoreExplorePosts } from "@/actions/post-actions";
+// import { Icons } from "@/components/shared/icons";
 
-  const fetchPosts = async ({ pageParam = 1 }: { pageParam?: number }) => {
-    const res = await fetchMoreExplorePosts({ page: pageParam });
-    return res;
-  };
+// export function InfiniteExploreScroll({
+//   initialPosts,
+// }: {
+//   initialPosts: PostExplorerType[] | [];
+// }) {
+//   const [ref, inView] = useInView();
 
-  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
-    useInfiniteQuery({
-      queryKey: ["explore-page-posts"],
-      queryFn: fetchPosts,
-      initialPageParam: 2,
-      getNextPageParam: (lastPage, allPages) =>
-        lastPage.length ? allPages.length + 1 : undefined,
-      initialData: {
-        pageParams: [1],
-        pages: [initialPosts],
-      },
-    });
+//   const fetchPosts = async ({ pageParam = 1 }: { pageParam?: number }) => {
+//     const res = await fetchMoreExplorePosts({ page: pageParam });
+//     return res;
+//   };
 
-  useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage();
-    }
-  }, [inView, hasNextPage, fetchNextPage]);
+//   const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+//     useInfiniteQuery({
+//       queryKey: ["explore-page-posts"],
+//       queryFn: fetchPosts,
+//       initialPageParam: 2,
+//       getNextPageParam: (lastPage, allPages) =>
+//         lastPage.length ? allPages.length + 1 : undefined,
+//       initialData: {
+//         pageParams: [1],
+//         pages: [initialPosts],
+//       },
+//     });
 
-  function getColumns(colIndex: number): PostExplorerType[] {
-    return data.pages.flatMap((page) =>
-      page.filter((post, index) => index % 3 === colIndex),
-    );
-  }
+//   useEffect(() => {
+//     if (inView && hasNextPage) {
+//       fetchNextPage();
+//     }
+//   }, [inView, hasNextPage, fetchNextPage]);
 
-  return (
-    <>
-      {[getColumns(0), getColumns(1), getColumns(2)].map((column, idx) => (
-        <div key={idx} className="flex flex-col gap-4">
-          {column.map((e) => (
-            <Image
-              key={e.media.asset_id}
-              src={e.media.secure_url}
-              placeholder="blur"
-              blurDataURL={e.media.blur_url}
-              sizes="100vw"
-              alt="image"
-              height={300}
-              width={500}
-            />
-          ))}
-        </div>
-      ))}
+//   function getColumns(colIndex: number): PostExplorerType[] {
+//     return data.pages.flatMap((page) =>
+//       page.filter((post, index) => index % 3 === colIndex),
+//     );
+//   }
 
-      <div
-        className="flex h-20 w-full items-center justify-center pb-32 pt-10"
-        ref={ref}
-      >
-        {isFetchingNextPage && (
-          <Icons.spinner className="mr-2 h-10 w-10 animate-spin" />
-        )}
-      </div>
-    </>
-  );
-}
+//   return (
+//     <>
+//       {[getColumns(0), getColumns(1), getColumns(2)].map((column, idx) => (
+//         <div key={idx} className="flex flex-col gap-4">
+//           {column.map((e) => (
+//             <Image
+//               key={e.media.asset_id}
+//               src={e.media.secure_url}
+//               placeholder="blur"
+//               blurDataURL={e.media.blur_url}
+//               sizes="100vw"
+//               alt="image"
+//               height={300}
+//               width={500}
+//             />
+//           ))}
+//         </div>
+//       ))}
+
+//       <div
+//         className="flex h-20 w-full items-center justify-center pb-32 pt-10"
+//         ref={ref}
+//       >
+//         {isFetchingNextPage && (
+//           <Icons.spinner className="mr-2 h-10 w-10 animate-spin" />
+//         )}
+//       </div>
+//     </>
+//   );
+// }
