e099f313270efa342200d17ead83eb49afa9108e
omprakash1353 <Omprakash13053@gmail.com>
2024-06-12 15:51:42 +0530

    0.0.10

--- src/app/(routes)/(nonchat)/explore/page.tsx
+++ src/app/(routes)/(nonchat)/explore/page.tsx
@@ -5,19 +5,19 @@ import { PostExplorerType } from "@/types/types";
 // import { InfiniteExploreScroll } from "./_components/infinite-explore-scroll";
 
 export default async function Page() {
-  // await dbConnect();
-  // const posts = (await PostModel.aggregate(postExploreAggregate(1, 12))) as
-  //   | PostExplorerType[]
-  //   | [];
+  await dbConnect();
+  const posts = (await PostModel.aggregate(postExploreAggregate(1, 12))) as
+    | PostExplorerType[]
+    | [];
 
-  // const parsedPosts = JSON.parse(JSON.stringify(posts));
+  const parsedPosts = JSON.parse(JSON.stringify(posts));
 
-  // if (!parsedPosts.length)
-  //   return (
-  //     <div className="h-full w-full p-10">
-  //       Sorry looks like we doesn't have enough data
-  //     </div>
-  //   );
+  if (!parsedPosts.length)
+    return (
+      <div className="h-full w-full p-10">
+        Sorry looks like we doesn't have enough data
+      </div>
+    );
 
   return (
     <div className="h-full w-full p-10">
