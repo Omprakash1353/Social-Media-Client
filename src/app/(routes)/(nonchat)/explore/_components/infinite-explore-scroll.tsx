ab2b769ae0325a05a65da8f6aaeef1fcbb6f6288
omprakash1353 <Omprakash13053@gmail.com>
2024-06-12 15:54:53 +0530

    0.0.11

--- src/app/(routes)/(nonchat)/explore/_components/infinite-explore-scroll.tsx
+++ src/app/(routes)/(nonchat)/explore/_components/infinite-explore-scroll.tsx
@@ -1,78 +1,78 @@
-// "use client";
+"use client";
 
-// import { useEffect } from "react";
-// import Image from "next/image";
-// import { useInView } from "react-intersection-observer";
-// import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-// import { PostExplorerType } from "@/types/types";
-// import { fetchMoreExplorePosts } from "@/actions/post-actions";
-// import { Icons } from "@/components/shared/icons";
+import { PostExplorerType } from "@/types/types";
+import { fetchMoreExplorePosts } from "@/actions/post-actions";
+import { Icons } from "@/components/shared/icons";
 
-// export function InfiniteExploreScroll({
-//   initialPosts,
-// }: {
-//   initialPosts: PostExplorerType[] | [];
-// }) {
-//   const [ref, inView] = useInView();
+export function InfiniteExploreScroll({
+  initialPosts,
+}: {
+  initialPosts: PostExplorerType[] | [];
+}) {
+  const [ref, inView] = useInView();
 
-//   const fetchPosts = async ({ pageParam = 1 }: { pageParam?: number }) => {
-//     const res = await fetchMoreExplorePosts({ page: pageParam });
-//     return res;
-//   };
+  const fetchPosts = async ({ pageParam = 1 }: { pageParam?: number }) => {
+    const res = await fetchMoreExplorePosts({ page: pageParam });
+    return res;
+  };
 
-//   const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
-//     useInfiniteQuery({
-//       queryKey: ["explore-page-posts"],
-//       queryFn: fetchPosts,
-//       initialPageParam: 2,
-//       getNextPageParam: (lastPage, allPages) =>
-//         lastPage.length ? allPages.length + 1 : undefined,
-//       initialData: {
-//         pageParams: [1],
-//         pages: [initialPosts],
-//       },
-//     });
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ["explore-page-posts"],
+      queryFn: fetchPosts,
+      initialPageParam: 2,
+      getNextPageParam: (lastPage, allPages) =>
+        lastPage.length ? allPages.length + 1 : undefined,
+      initialData: {
+        pageParams: [1],
+        pages: [initialPosts],
+      },
+    });
 
-//   useEffect(() => {
-//     if (inView && hasNextPage) {
-//       fetchNextPage();
-//     }
-//   }, [inView, hasNextPage, fetchNextPage]);
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
-//   function getColumns(colIndex: number): PostExplorerType[] {
-//     return data.pages.flatMap((page) =>
-//       page.filter((post, index) => index % 3 === colIndex),
-//     );
-//   }
+  function getColumns(colIndex: number): PostExplorerType[] {
+    return data.pages.flatMap((page) =>
+      page.filter((post, index) => index % 3 === colIndex),
+    );
+  }
 
-//   return (
-//     <>
-//       {[getColumns(0), getColumns(1), getColumns(2)].map((column, idx) => (
-//         <div key={idx} className="flex flex-col gap-4">
-//           {column.map((e) => (
-//             <Image
-//               key={e.media.asset_id}
-//               src={e.media.secure_url}
-//               placeholder="blur"
-//               blurDataURL={e.media.blur_url}
-//               sizes="100vw"
-//               alt="image"
-//               height={300}
-//               width={500}
-//             />
-//           ))}
-//         </div>
-//       ))}
+  return (
+    <>
+      {[getColumns(0), getColumns(1), getColumns(2)].map((column, idx) => (
+        <div key={idx} className="flex flex-col gap-4">
+          {column.map((e) => (
+            <Image
+              key={e.media.asset_id}
+              src={e.media.secure_url}
+              placeholder="blur"
+              blurDataURL={e.media.blur_url}
+              sizes="100vw"
+              alt="image"
+              height={300}
+              width={500}
+            />
+          ))}
+        </div>
+      ))}
 
-//       <div
-//         className="flex h-20 w-full items-center justify-center pb-32 pt-10"
-//         ref={ref}
-//       >
-//         {isFetchingNextPage && (
-//           <Icons.spinner className="mr-2 h-10 w-10 animate-spin" />
-//         )}
-//       </div>
-//     </>
-//   );
-// }
+      <div
+        className="flex h-20 w-full items-center justify-center pb-32 pt-10"
+        ref={ref}
+      >
+        {isFetchingNextPage && (
+          <Icons.spinner className="mr-2 h-10 w-10 animate-spin" />
+        )}
+      </div>
+    </>
+  );
+}
--- src/app/(routes)/(nonchat)/explore/page.tsx
+++ src/app/(routes)/(nonchat)/explore/page.tsx
@@ -2,13 +2,11 @@ import { dbConnect } from "@/lib/dbConnection";
 import { PostModel } from "@/models/post.model";
 import { postExploreAggregate } from "@/lib/aggregates";
 import { PostExplorerType } from "@/types/types";
-// import { InfiniteExploreScroll } from "./_components/infinite-explore-scroll";
+import { InfiniteExploreScroll } from "./_components/infinite-explore-scroll";
 
 export default async function Page() {
   await dbConnect();
-  const posts = (await PostModel.aggregate(postExploreAggregate(1, 12))) as
-    | PostExplorerType[]
-    | [];
+  const posts = await PostModel.aggregate(postExploreAggregate(1, 12));
 
   const parsedPosts = JSON.parse(JSON.stringify(posts));
 
@@ -22,7 +20,7 @@ export default async function Page() {
   return (
     <div className="h-full w-full p-10">
       <div className="grid grid-cols-3 gap-4">
-        {/* <InfiniteExploreScroll initialPosts={parsedPosts} /> */}
+        <InfiniteExploreScroll initialPosts={parsedPosts} />
       </div>
     </div>
   );
